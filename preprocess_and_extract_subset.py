f75287e72cc1ca90c0345a5121abb548c500f7c4
Maxim Luchiacniuc <76953276+makasim98@users.noreply.github.com>
2024-02-15 16:17:13 -0500

    small corrections on folder names

--- preprocess_and_extract_subset.py
+++ preprocess_and_extract_subset.py
@@ -7,52 +7,51 @@ import imutils
 import random
 import pickle
 
-DATASET_A_DIR = Path.cwd() / "DataSet_B"
+DATASET_A_DIR = Path.cwd() / "DataSet_A"
 LABELS = ["Cercle2", "Cercle3", "Diamant2", "Diamant5", "Hexagone3", "Hexagone4", "Triangle3", "Triangle5"]
 
-def pretreat_subset(pretreated_subset_path: Path):
+def prepare_subset(pretreated_subset_path: Path):
     
-    form_dir = ""
-    form_subdir = ""
-    file_name = ""
-    
+    # For each shape folder
     for first_level in DATASET_A_DIR.glob('*'):
         if first_level.is_dir():
-            form_dir = first_level.name
+            
+            # For each chape subfolder
             for second_level in first_level.glob('*'):
                 if second_level.is_dir():
-                    form_subdir = second_level.name
-                    class_num = LABELS.index(form_subdir) if form_subdir in LABELS else -1
-                    if(class_num >= 0):
-                        print(f"{label} {class_num}")
+                    label = second_level.name
+                    class_num = LABELS.index(label) if label in LABELS else -1
                     
-                    for file in second_level.glob('*'):
-                        if file.is_file():
-                            file_name = file.name
-                            # preprocess image
-                            image = cv2.imread(file)
-                            processed_image = preprocess_image(image)
-                            
-                            # extract maker from that image
-                            new_image = extract_marker(image, processed_image)
-                            
-                            # save extracted to processed dataset
-                            subset_dir = pretreated_subset_path / form_dir / form_subdir / file_name
-                            save_image_to(subset_dir, new_image)
+                    # If said shape label was chosen for subset
+                    if(class_num >= 0):
+                        
+                        # For each file in shape subfolder
+                        for file in second_level.glob('*'):
+                            if file.is_file():
+                                # preprocess image
+                                image = cv2.imread(file)
+                                processed_image = preprocess_image(image)
+                                
+                                # extract maker from that image
+                                new_image = extract_marker(image, processed_image)
+                                
+                                # save extracted to processed dataset
+                                subset_dir = pretreated_subset_path / first_level.name / second_level.name / file.name
+                                save_image_to(subset_dir, new_image)
                             
 
 def preprocess_image(image):
-    pass
+    return 0
 
 def extract_marker(originalImage, processedImage):
-    pass
+    return 0
 
 def save_image_to(dir, image):
     cv2.imwrite(dir, image)
 
 
 def program():
-    # path = Path.cwd() / "Preprocessed_DataSet_B"
-    # pretreat_subset(path)    
+    path = Path.cwd() / "Processed_DataSet_A"
+    prepare_subset(path)
 
 program()
