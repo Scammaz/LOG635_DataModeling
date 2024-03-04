9fac39127ad4ff9a3cafa270ad39dbb72dbadf7b
Maxim Luchiacniuc <76953276+makasim98@users.noreply.github.com>
2024-03-04 16:57:00 -0500

    upd

--- ai_test.py
+++ ai_test.py
@@ -83,7 +83,7 @@ def NN_test(nombre_de_couches_cachees=5, nombre_de_neurones_par_couche=1024, tau
         nb_inputs=1600,
         nb_outputs=8,
         nb_hidden_layers=2, #2
-        nb_nodes_per_layer=256, #2048
+        nb_nodes_per_layer=256, #1024
         learning_rate=0.01
     )
     
@@ -126,21 +126,26 @@ def test():
     list_neurones_par_couche = [512, 1024, 2048]
     list_taux_dapprentissage = [0.01, 0.02, 0.05, 0.1, 0.5]
     list_iterations = [100, 200, 300, 400, 500]
-    result = []
-
+    percentageDone = len(list_couche_cachee) * len(list_neurones_par_couche) * len(list_taux_dapprentissage) * len(list_iterations)
+    x = 0
+    headerBool = True
     for couche_cachee in list_couche_cachee:
         for neurones_par_couche in list_neurones_par_couche:
             for taux_dapprentissage in list_taux_dapprentissage:
                 for iterations in list_iterations:
-                    #print(f"couche_cachee = {couche_cachee}, neurones_par_couche = {neurones_par_couche}, taux_dapprentissage = {taux_dapprentissage}, iterations = {iterations}")
+                    print(f"couche_cachee = {couche_cachee}, neurones_par_couche = {neurones_par_couche}, taux_dapprentissage = {taux_dapprentissage}, iterations = {iterations}")
                     accuracy_score = NN_test(couche_cachee, neurones_par_couche, taux_dapprentissage, iterations)                   
                     precision = accuracy_score[0]
                     rappel = accuracy_score[1]  
                     f1 = accuracy_score[2]
 
-                    result.append([couche_cachee, neurones_par_couche, taux_dapprentissage, iterations, precision, rappel, f1])
+                    result = [[couche_cachee, neurones_par_couche, taux_dapprentissage, iterations, precision, rappel, f1]]
+                    df = pd.DataFrame(result, columns=['Couche Cachee', 'Neuronne pc', 'Taux Aprentissage', 'Iterations', 'Precision', 'Rappel', 'F1'])
+                    df.to_csv('resultsAiModel.csv', mode='a', header=headerBool, index=False)
+                    headerBool = False
+                    x += 1
+                    print(f"Percentage done: {x/percentageDone*100} %")
 
-    df = pd.DataFrame(results, columns=['Couche Cachee', 'Neuronne pc', 'Taux Aprentissage', 'Iterations', 'Precision', 'Rappel', 'F1'])
-    df.to_csv('resultsAiModel.csv', index=False    )               
 
+test()   
     
