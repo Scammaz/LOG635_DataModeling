fc516b042f347d89306a8b0049d485ccf0fe5366
Maxim Luchiacniuc <76953276+makasim98@users.noreply.github.com>
2024-03-07 14:28:36 -0500

    Add sequential/multithread hyper-ps testing scripts

--- NN_bulk_testing_multiprocess.py
+++ NN_bulk_testing_multiprocess.py
@@ -17,11 +17,8 @@ from test_RN import NeuralNetwork2
 
 label_encoder = LabelBinarizer()
 test_res_path = lambda h, o : f'test_results/{h}_{o}/'
-test_len = 0
-test_progress = 0
-
     
-def testV2_init(X_data, Y_labels, hidden_fn='sigmoid', out_fn='sigmoid'):
+def testV2_init(X_data, Y_labels, hidden_fn='sigmoid', out_fn='sigmoid', nb_parallel_jobs=1):
     path = f'{test_res_path(hidden_fn, out_fn)}resultsAiTraining.csv'
     
     nb_hidden_layers = [1, 2, 3, 4]
@@ -29,23 +26,31 @@ def testV2_init(X_data, Y_labels, hidden_fn='sigmoid', out_fn='sigmoid'):
     learning_rates = [0.01, 0.02, 0.05, 0.1]
     epochs = [500, 1000, 2500, 5000]
     
+    procs = []
     csv_header = True
     start_time = time.time()
     
-    global test_len
     test_len = len(nb_hidden_layers) * len(nb_node_per_layer) * len(learning_rates) * len(epochs)
+    test_progress = Value('i', 0)
+    semaphore = Semaphore(nb_parallel_jobs)
     
     for hd in nb_hidden_layers:
         for npl in nb_node_per_layer:
             for lr in learning_rates:
                 for e in epochs:
-                    testV2(copy.deepcopy(X_data), copy.deepcopy(Y_labels),hd, npl, lr, e, hidden_fn, out_fn, path, csv_header)
+                    semaphore.acquire()
+                    p = Process(target=testV2, args=[copy.deepcopy(X_data), copy.deepcopy(Y_labels),hd, npl, lr, e, hidden_fn, out_fn, path, csv_header, test_len, test_progress, semaphore])
                     if csv_header:
                         csv_header = False
+                    procs.append(p)
+                    p.start()
+                    
+    for p in procs:
+        p.join()
         
     print(f"Tests finished in {time.time() - start_time} seconds")     
 
-def testV2(X, Y, hidden_layers, node_per_layer, learn_rate, epochs, hidden_fn, out_fn, res_path, header_bool):
+def testV2(X, Y, hidden_layers, node_per_layer, learn_rate, epochs, hidden_fn, out_fn, res_path, header_bool, test_len, test_progress, semaphore):
     print(f"hidden_layers = {hidden_layers}, node_per_layer = {node_per_layer}, learn_rate = {learn_rate}, epochs = {epochs}")
     name = f'{hidden_fn}_{out_fn}_{hidden_layers}_{node_per_layer}_{learn_rate}_{epochs}'
     
@@ -74,9 +79,8 @@ def testV2(X, Y, hidden_layers, node_per_layer, learn_rate, epochs, hidden_fn, o
     df = pd.DataFrame(result, columns=['Hidden Layers', 'Nodes per Layer', 'Learning Rate', 'Epochs', 'Loss', 'Precision', 'Recall', 'F1', 'Training_time (sec)'])
     df.to_csv(res_path, mode='a', header=header_bool, index=False)
     
-    global test_progress
-    test_progress += 1
-    print(f"Percentage done: {(test_progress/test_len)*100} %")
+    test_progress.value += 1
+    print(f"Percentage done: {(test_progress.value/test_len)*100} %")
 
 
 def NN_test(features, outputs, validation=None, validation_arg=0.1, hidden_layers=2, nodes_per_layer=512, learning_rate=0.2, epochs=500, hidden_activation_func='sigmoid', output_activation_func='sigmoid', suppress_log=True):
@@ -193,7 +197,7 @@ if __name__ == "__main__":
     with open("Y.pkl", "rb") as db:
         outputs = pickle.load(db)
     
-    testV2_init(features, outputs, hidden_fn='sigmoid', out_fn='sigmoid')
+    testV2_init(features, outputs, hidden_fn='sigmoid', out_fn='sigmoid', nb_parallel_jobs=2)
     
     # results = NN_test(
     #     features=features, 
